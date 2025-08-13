8f3168bfe9fb6ee8862b163fced027e50b1200f5
Abel Nunez <abelkevin13@gmail.com>
2025-08-12 23:10:46 -0500

    first commit

---
+++ src/main/java/com/enterprise/servicecenter/ServiceCenterApplication.java
@@ -0,0 +1,13 @@
+package com.enterprise.servicecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCenterApplication.class, args);
+	}
+
+}
