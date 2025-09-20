cae23a27ed39b7aa2c628fc8b7039cea75d1df03
Abel Nunez <abelkevin13@gmail.com>
2025-09-20 14:38:15 -0500

    chore: ping server every 10 minutes

--- src/main/java/com/enterprise/servicecenter/infrastructure/adapter/in/web/TestController.java
+++ src/main/java/com/enterprise/servicecenter/infrastructure/adapter/in/web/TestController.java
@@ -1,15 +1,18 @@
 package com.enterprise.servicecenter.infrastructure.adapter.in.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/hello")
+@Slf4j
 public class TestController {
 
     @GetMapping
     public String sayHello() {
+        log.info("hello");
         return "Hello!";
     }
 
