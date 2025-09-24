b798243760891186905b3b2954f7fb0d613671e2
harolddirkz <harold.dirkz01@gmail.com>
2025-09-24 17:15:49 -0500

    Modifying the util class according to ISOString for Date

--- src/main/java/com/enterprise/servicecenter/common/util/DateUtil.java
+++ src/main/java/com/enterprise/servicecenter/common/util/DateUtil.java
@@ -1,11 +1,13 @@
 package com.enterprise.servicecenter.common.util;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtil {
 
     public static LocalDateTime toLocalDateTime(String date) {
-        return LocalDateTime.parse(date, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        OffsetDateTime offsetDateTime = OffsetDateTime.parse(date);
+        return offsetDateTime.toLocalDateTime();
     }
 }
