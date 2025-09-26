91d62877ea18ad0e2fda4000e45dc72dbc225c28
rod12151 <rodmel.calderon.27@unsch.edu.pe>
2025-09-26 06:20:09 -0500

    feat:add producto and search all functionality

--- src/main/java/com/enterprise/servicecenter/infrastructure/repository/jpa/JpaProductRepository.java
+++ src/main/java/com/enterprise/servicecenter/infrastructure/repository/jpa/JpaProductRepository.java
@@ -1,7 +1,11 @@
 package com.enterprise.servicecenter.infrastructure.repository.jpa;
 
 import com.enterprise.servicecenter.domain.model.ProductDao;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaProductRepository extends JpaRepository<ProductDao, String> {
+  @Override
+  Page<ProductDao> findAll(Pageable pageable);
 }
