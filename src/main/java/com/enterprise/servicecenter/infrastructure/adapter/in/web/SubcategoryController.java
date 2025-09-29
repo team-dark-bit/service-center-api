a69ee79d1328e3417ae950a667525da456be3e9a
Abel Nunez <abelkevin13@gmail.com>
2025-09-29 06:37:44 -0500

    chore: rename endpoint from /subcategory to /subcategories

--- src/main/java/com/enterprise/servicecenter/infrastructure/adapter/in/web/SubcategoryController.java
+++ src/main/java/com/enterprise/servicecenter/infrastructure/adapter/in/web/SubcategoryController.java
@@ -17,10 +17,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/subcategory")
+@RequestMapping("/subcategories")
 @Slf4j
 @Validated
 @RequiredArgsConstructor
